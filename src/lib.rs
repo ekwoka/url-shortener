9febf9694aae788561e66b4566853d3a62b3a28f
Eric Kwoka <43540491+ekwoka@users.noreply.github.com>
2023-05-10 21:54:57 +0400

    :recycle: Extracts get redirect to module

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,10 @@
 use serde::{Deserialize, Serialize};
-use std::convert::Infallible;
 use surrealdb::engine::remote::ws::{Client, Ws};
 use surrealdb::sql::Thing;
 use surrealdb::Surreal;
-use warp::{http::Response, Filter};
+use warp::Filter;
 
-use crate::routes::make_shortener;
+use crate::routes::{get_redirect, make_shortener};
 
 mod routes;
 
@@ -24,26 +23,7 @@ struct Record {
 pub async fn run() -> surrealdb::Result<()> {
     let db = Surreal::new::<Ws>("127.0.0.1:8000").await?;
 
-    fn with_db(db: Db) -> impl Filter<Extract = (Db,), Error = std::convert::Infallible> + Clone {
-        warp::any().map(move || db.clone())
-    }
-    async fn get_url(id: String, _db: Db) -> Result<Response<String>, Infallible> {
-        let Ok(_id) = &id.parse::<i32>() else {
-            return Ok(Response::builder()
-                .status(200)
-                .body("Invalid URL - Needs to be Number".to_string()).unwrap());
-        };
-        Ok(Response::builder()
-            .status(200)
-            .body("Invalid URL - Not Registered Yet".to_string())
-            .unwrap())
-    }
-
-    let get_shortened = warp::path!(String)
-        .and(with_db(db.clone()))
-        .and_then(get_url);
-
-    let shortener = make_shortener(db.clone()).or(get_shortened);
+    let shortener = make_shortener(db.clone()).or(get_redirect(db));
 
     println!("Listening on port 8080");
     warp::serve(shortener).run(([0, 0, 0, 0], 8080)).await;
--- src/routes.rs
+++ src/routes.rs
@@ -1,6 +1,6 @@
 use std::convert::Infallible;
 
-use warp::{filters, path::FullPath, Filter};
+use warp::{filters, http::Response, path::FullPath, Filter};
 
 use crate::{Record, Redirect};
 
@@ -26,5 +26,27 @@ pub fn make_shortener(db: crate::Db) -> filters::BoxedFilter<(String,)> {
         .and(with_full())
         .and(warp::any().map(move || db.clone()))
         .and_then(make_url)
+        .boxed()
+}
+
+pub fn get_redirect(db: crate::Db) -> filters::BoxedFilter<(Response<String>,)> {
+    fn with_db(db: crate::Db) -> filters::BoxedFilter<(crate::Db,)> {
+        warp::any().map(move || db.clone()).boxed()
+    }
+    async fn get_url(id: String, _db: crate::Db) -> Result<Response<String>, Infallible> {
+        let Ok(_id) = &id.parse::<i32>() else {
+            return Ok(Response::builder()
+                .status(200)
+                .body("Invalid URL - Needs to be Number".to_string()).unwrap());
+        };
+        Ok(Response::builder()
+            .status(200)
+            .body("Invalid URL - Not Registered Yet".to_string())
+            .unwrap())
+    }
+
+    warp::path!(String)
+        .and(with_db(db))
+        .and_then(get_url)
         .boxed()
 }
